c40fb933fa325b7e24a32eebe8d347def3236514
aeisele14 <eisele.alex@gmail.com>
2014-06-30 22:38:33 -0500

    Added xml parsing and display parsed data in list view under sodex fragment. Network connectivity check refactor.

--- HastingsCollegeProject/HastingsCollege/src/main/java/edu/hastings/hastingscollege/navdrawerfragments/FragmentAthletics.java
+++ HastingsCollegeProject/HastingsCollege/src/main/java/edu/hastings/hastingscollege/navdrawerfragments/FragmentAthletics.java
@@ -9,15 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
-import edu.hastings.hastingscollege.Connection;
+
 import edu.hastings.hastingscollege.R;
 
 public class FragmentAthletics extends Fragment {
 
     WebView myWebView;
 
-    public static Fragment newInstance(Context context) { return new FragmentAthletics(); }
+    public static Fragment newInstance(Context context) {
+        return new FragmentAthletics();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,57 +29,43 @@ public class FragmentAthletics extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        myWebView = (WebView) getView().findViewById(R.id.webview);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new MyWebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                //hide loading image
+                getView().findViewById(R.id.athleticsProgress).setVisibility(View.GONE);
+                //Show webview
+                getView().findViewById(R.id.webview).setVisibility(View.VISIBLE);
+            }
+        });
+        myWebView.loadUrl(getString(R.string.athletics_url));
+        myWebView.getSettings().setBuiltInZoomControls(true);
 
-        boolean connection = new Connection().hasConnection(getActivity());
+        myWebView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    WebView webView = (WebView) view;
 
-        if (connection)
-        {
-            myWebView = (WebView) getView().findViewById(R.id.webview);
-            myWebView.getSettings().setJavaScriptEnabled(true);
-            myWebView.setWebViewClient(new MyWebViewClient(){
-                @Override
-                public void onPageFinished(WebView view, String url) {
-                    //hide loading image
-                    getView().findViewById(R.id.athleticsProgress).setVisibility(View.GONE);
-                    //Show webview
-                    getView().findViewById(R.id.webview).setVisibility(View.VISIBLE);
-                }
-            });
-            myWebView.loadUrl(getString(R.string.athletics_url));
-            myWebView.getSettings().setBuiltInZoomControls(true);
-
-            myWebView.setOnKeyListener(new View.OnKeyListener() {
-                @Override
-                public boolean onKey(View view, int i, KeyEvent event) {
-                    if (event.getAction() == KeyEvent.ACTION_DOWN)
-                    {
-                        WebView webView = (WebView) view;
-
-                        switch (i)
-                        {
-                            case KeyEvent.KEYCODE_BACK:
-                                if(webView.canGoBack())
-                                {
-                                    webView.goBack();
-                                    return true;
-                                }
-                                break;
-                        }
+                    switch (i) {
+                        case KeyEvent.KEYCODE_BACK:
+                            if (webView.canGoBack()) {
+                                webView.goBack();
+                                return true;
+                            }
+                            break;
                     }
-                    return false;
                 }
-            });
-        }
-        else
-        {
-            Toast.makeText(getActivity(),
-                "Check Your Internet Connection, Wifi Or Mobile Data Must Be Enabled",
-                Toast.LENGTH_LONG).show();
-        }
+                return false;
+            }
+        });
     }
 
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.athletics, container, false);
         return root;
     }
@@ -87,7 +74,7 @@ public class FragmentAthletics extends Fragment {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             //if (Uri.parse(url).getHost().equals("http://catchfiredevprojects.com/hastings/homepage.html#"))
-                return false;
+            return false;
             //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
             //startActivity(intent);
             //return true;
