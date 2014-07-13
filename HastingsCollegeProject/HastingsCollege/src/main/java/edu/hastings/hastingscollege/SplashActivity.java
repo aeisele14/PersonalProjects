4264ec487fee0e3d80e26cebaff82404600fa224
aeisele14 <eisele.alex@gmail.com>
2014-07-13 16:48:19 -0500

    changed splash screen transition and added preference

--- HastingsCollegeProject/HastingsCollege/src/main/java/edu/hastings/hastingscollege/SplashActivity.java
+++ HastingsCollegeProject/HastingsCollege/src/main/java/edu/hastings/hastingscollege/SplashActivity.java
@@ -2,8 +2,10 @@ package edu.hastings.hastingscollege;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.view.Window;
 
 public class SplashActivity extends Activity {
@@ -13,22 +15,33 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);    // Removes title bar
-        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,     WindowManager.LayoutParams.FLAG_FULLSCREEN);    // Removes notification bar
-
         setContentView(R.layout.splash);
+    }
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
-                finish();
-                // transition from splash to main menu
-                overridePendingTransition(R.anim.zoom_enter,
-                        R.anim.zoom_exit);
-            }
-        }, SPLASH_TIME_OUT);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isSplashEnabled = sp.getBoolean("isSplashEnabled", true);
+
+        if (isSplashEnabled) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    SplashActivity.this.finish();
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    SplashActivity.this.startActivity(i);
+                    // transition from splash to main menu
+                    overridePendingTransition(R.anim.left,
+                            R.anim.right);
+                }
+            }, SPLASH_TIME_OUT);
+        }
+        else {
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            SplashActivity.this.startActivity(i);
+            finish();
+        }
     }
 }
